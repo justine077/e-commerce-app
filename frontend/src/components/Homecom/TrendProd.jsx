7887d4a2be4336cd4e84c3cab21397b9de053fe9
Tey <justinevillanueva090794@gmail.com>
2022-12-19 15:05:04 +0800

    TrendProducts

--- frontend/src/components/Homecom/TrendProd.jsx
+++ frontend/src/components/Homecom/TrendProd.jsx
@@ -8,14 +8,22 @@ const TrendProd = () => {
         <div className='max-w-screen-md mx-auto mt-10'>
             <h1 className='relative font-bold text-6xl text-center font-body text-gray-800 ugd-title'><span>Trending Products</span></h1>
         </div>
-        <div class="grid grid-flow-col auto-cols-max justify-center mt-10 grid-cols-4 grid-rows-2 mx-auto max-w-max">
-            {
+       <div>
+      <section class="bg-white dark:bg-gray-900">
+    
+    <div class="container px-6 py-8 mx-auto">
+                  <div class="grid gap-3 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                      {
                 Data.map(product =>{
                     
                 return <ProductItem className="mx-auto justify-center" key={product.id} product={product} />
                 })
             }
+            
         </div>
+    </div>
+</section>
+    </div>
     </div>
   )
 }
