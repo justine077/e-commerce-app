a60edececd15cd72feedaa3d0be63a36fd056a47
Tey <justinevillanueva090794@gmail.com>
2022-12-27 14:22:36 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import Login from './pages/Login/Login';
-import Signup from './pages/Signup';
 import Footer from './Footer/Footer';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/Product' element={<Product />} />
           <Route path='/Login' element={<Login />} />
-          <Route path='/Signup' element={<Signup />} />
         </Routes>
       </Router>
       <Footer />
